ef45ce8fe39100ceb17dc301180e32cc99604501
200016harshjain <44161491+200016harshjain@users.noreply.github.com>
2024-03-18 16:47:48 +0530

    remove directory

--- apps/directory/test/index.spec.js
+++
@@ -1,9 +0,0 @@
-import assert from 'assert';
-
-describe('PeopleRepository', function() {
-  describe('#dummyTest()', function() {
-    it('should always pass', function() {
-      assert.equal(true, true);
-    });
-  });
-});
